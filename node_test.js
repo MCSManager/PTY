d6c15b0a28e67cb1a573f410701aa77fd98e5968
Suwings <Suwings@outlook.com>
2022-07-01 23:45:23 +0800

    Feat: 新增性能测试

--- node_test.js
+++ node_test.js
@@ -26,21 +26,43 @@ p.stdout.on("data", (v) => {
 
 process.stdin.on("data", (v) => {
     let text = v.toString()
+
+    if (text.includes("performance")) {
+        let i = 0
+        setInterval(() => {
+            text = JSON.stringify({
+                type: 1,
+                data: `echo Hello1: ${i}\r\n`
+            })
+            p.stdin.write(text + "\n");
+            i++
+        })
+        setInterval(() => {
+            text = JSON.stringify({
+                type: 1,
+                data: `echo Hello2: ${i}\r\n`
+            })
+            i++
+            p.stdin.write(text + "\n");
+        })
+        return
+    }
+
     if (text.toString().includes("exit0")) {
         return p.stdin.write(JSON.stringify({
             type: 3,
             data: ""
-        })+"\n");
+        }) + "\n");
     }
     if (text.toString().includes("resize")) {
         const arr = text.split(" ").slice(1)
-        console.log("RESIZE WIN:",`${arr[0]} ${arr[1]}`)
+        console.log("RESIZE WIN:", `${arr[0]} ${arr[1]}`)
         return p.stdin.write(JSON.stringify({
             type: 2,
             data: `${arr[0]},${arr[1]}`
-        })+"\n");
+        }) + "\n");
     }
-     text = JSON.stringify({
+    text = JSON.stringify({
         type: 1,
         data: v.toString()
     })
