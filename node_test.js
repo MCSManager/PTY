c19affee318c4acbe131552e44cd72c4fd1011b1
zijiren233 <pyh1670605849@gmail.com>
2022-07-12 14:52:15 +0800

    Fix: 删除 node_test type模式

--- node_test.js
+++ node_test.js
@@ -1,28 +1,12 @@
 const { spawn } = require("child_process");
 
-const command = JSON.stringify([
-    '"C:\\Program Files\\Java\\jre1.8.0_333\\bin\\java.exe"',
-    "-jar",
-    "ser.jar",
-    "nogui",
-]);
+const command = JSON.stringify(["cmd.exe"]);
 
-const p = spawn(
-    "./pty.exe",
-    [
-        "-dir",
-        "C:\\Users\\zijiren\\Desktop\\ser  aasd",
-        "-cmd",
-        command,
-        "-size",
-        "50,50",
-    ],
-    {
-        cwd: ".",
-        stdio: "pipe",
-        windowsHide: true,
-    }
-);
+const p = spawn("./pty.exe", ["-dir", ".", "-cmd", command, "-size", "50,50"], {
+    cwd: ".",
+    stdio: "pipe",
+    windowsHide: true,
+});
 
 if (!p.pid) throw new Error("[DEBUG] 启动失败");
 
@@ -36,51 +20,5 @@ p.stdout.on("data", (v) => {
 
 process.stdin.on("data", (v) => {
     let text = v.toString();
-
-    if (text.includes("performance")) {
-        let i = 0;
-        setInterval(() => {
-            text = JSON.stringify({
-                type: 1,
-                data: `echo Hello1: ${i}\r\n`,
-            });
-            p.stdin.write(text + "\n");
-            i++;
-        });
-        setInterval(() => {
-            text = JSON.stringify({
-                type: 1,
-                data: `echo Hello2: ${i}\r\n`,
-            });
-            i++;
-            p.stdin.write(text + "\n");
-        });
-        return;
-    }
-
-    if (text.toString().includes("exit0")) {
-        console.log("EXIT");
-        return p.stdin.write(
-            JSON.stringify({
-                type: 3,
-                data: "",
-            }) + "\n"
-        );
-    }
-    if (text.toString().includes("resize")) {
-        const arr = text.split(" ").slice(1);
-        console.log("RESIZE WIN:", `${arr[0]} ${arr[1]}`);
-        return p.stdin.write(
-            JSON.stringify({
-                type: 2,
-                data: `${arr[0]},${arr[1]}`,
-            }) + "\n"
-        );
-    }
-    text = JSON.stringify({
-        type: 1,
-        data: v.toString(),
-    });
-    console.log("[DEBUG] Node >>>>> Go", text);
-    p.stdin.write(text + "\n");
+    p.stdin.write(text);
 });
